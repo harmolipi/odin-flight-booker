19221086079975251214e067b3d0131846815150
harmolipi <nikob381@gmail.com>
2021-09-24 16:51:06 -0400

    Seeding with 2 airports and 2 flights

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Airport.destroy_all
+Flight.destroy_all
+
+DURATIONS = {
+  SFO: {
+    JFK: 20_580
+  },
+  JFK: {
+    SFO: 17_640
+  }
+}.freeze
+
+current_time = Time.now
+Airport.create([{ code: 'SFO' }, { code: 'NYC' }])
+Flight.create(from_airport: Airport.first, to_airport: Airport.last, duration: DURATIONS[:SFO][:JFK], departure: current_time)
+Flight.create(from_airport: Airport.last, to_airport: Airport.first, duration: DURATIONS[:JFK][:SFO], departure: current_time + 7.days)
