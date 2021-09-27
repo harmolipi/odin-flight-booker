168555aff302294a65ea406e9d42f3f92c1569d7
harmolipi <nikob381@gmail.com>
2021-09-27 17:12:11 -0400

    Controller and views to search and display flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,16 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @params = params[:flight] ? params[:flight] : {}
+    @flights = params[:flight] ? Flight.where(from_airport: flight_params[:from_airport], to_airport: flight_params[:to_airport], departure_date: flight_params[:departure_date]).order(:departure_date, :departure_time) : Flight.all.order(:departure_date, :departure_time)
+    # binding.pry
+    @flight = Flight.new
+    @airports = Airport.all
+    @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:from_airport, :to_airport, :departure_date)
   end
 end
