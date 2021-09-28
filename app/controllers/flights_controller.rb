2b1712383cc5db48760c60ce8759c23b932e7454
harmolipi <nikob381@gmail.com>
2021-09-28 13:10:41 -0400

    Modified flight display and messages

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,8 @@
 class FlightsController < ApplicationController
   def index
-    @params = params[:flight] ? params[:flight] : {}
-    @flights = params[:flight] ? Flight.where(from_airport: flight_params[:from_airport], to_airport: flight_params[:to_airport], departure_date: flight_params[:departure_date]).order(:departure_date, :departure_time) : Flight.all.order(:departure_date, :departure_time)
-    # binding.pry
+    @params = params[:flight] || {}
+    @flights = params[:flight] ? Flight.where(from_airport: flight_params[:from_airport], to_airport: flight_params[:to_airport], departure_date: flight_params[:departure_date]).order(:departure_date, :departure_time) : {}
     @flight = Flight.new
-    @airports = Airport.all
     @airport_options = Airport.all.map { |airport| [airport.code, airport.id] }
   end
 
