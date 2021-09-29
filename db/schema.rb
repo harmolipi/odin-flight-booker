9a134cedeeb682c3b29ce46e69196684e7e2b6d7
harmolipi <nikob381@gmail.com>
2021-09-29 16:29:48 -0400

    Created Booking model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_24_183414) do
+ActiveRecord::Schema.define(version: 2021_09_29_193409) do
 
   create_table "airports", force: :cascade do |t|
     t.string "code"
@@ -19,17 +19,36 @@ ActiveRecord::Schema.define(version: 2021_09_24_183414) do
     t.index ["code"], name: "index_airports_on_code", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id", null: false
+    t.integer "passenger_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+    t.index ["passenger_id"], name: "index_bookings_on_passenger_id"
+  end
+
   create_table "flights", force: :cascade do |t|
     t.integer "from_airport_id", null: false
     t.integer "to_airport_id", null: false
     t.datetime "departure"
     t.integer "duration"
+    t.string "flight_number"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["from_airport_id"], name: "index_flights_on_from_airport_id"
     t.index ["to_airport_id"], name: "index_flights_on_to_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "bookings", "flights"
+  add_foreign_key "bookings", "passengers"
   add_foreign_key "flights", "airports", column: "from_airport_id"
   add_foreign_key "flights", "airports", column: "to_airport_id"
 end
