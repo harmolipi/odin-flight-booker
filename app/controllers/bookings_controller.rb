46357d91b88281dcda2d10b9a3416d49c829e888
harmolipi <nikob381@gmail.com>
2021-10-01 17:48:10 -0400

    Created Booking form with nested Passenger fields

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,27 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Flight.find(params[:flight_id]).bookings.new
+    params[:passenger_count].to_i.times { @booking.passengers.new }
+    # @booking.passengers.build
+    # binding.pry
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render root_path
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def booking_params
+    params.fetch(:booking, {}).permit(:flight_id, :passenger_count, passengers_attributes: [:name, :email])
+  end
 end
