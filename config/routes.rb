5e238ec097412048a25a2cb9b81250cdc8dd5f19
harmolipi <nikob381@gmail.com>
2021-10-01 18:03:36 -0400

    Tightened up routes to only include necessary ones

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'flights#index'
-  resources :flights
-  resources :airports
-  resources :bookings
-  resources :passengers
+  resources :flights, only: [:index]
+  resources :bookings, only: [:new, :create, :show]
+  resources :passengers, only: [:create]
 end
