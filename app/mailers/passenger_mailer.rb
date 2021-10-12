e62473ccab3318ce32da90a4f9776313c2c17c3e
harmolipi <nikob381@gmail.com>
2021-10-12 10:54:37 -0400

    Set up #thank_you_email

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,8 @@
 class PassengerMailer < ApplicationMailer
+  def thank_you_email
+    @passenger = params[:passenger]
+    @booking = @passenger.booking
+    @flight = @booking.flight
+    mail(to: @passenger.email, subject: "Booking confirmed for flight: #{@flight.flight_number}")
+  end
 end
