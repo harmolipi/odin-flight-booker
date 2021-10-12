a08e5017578739b53350371d0e723458110fbd38
harmolipi <nikob381@gmail.com>
2021-10-12 16:06:11 -0400

    Added ability to manually pass in passenger

--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,6 +1,6 @@
 class PassengerMailer < ApplicationMailer
-  def thank_you_email
-    @passenger = params[:passenger]
+  def thank_you_email(passenger = nil)
+    @passenger = params.nil? ? passenger : params[:passenger]
     @booking = @passenger.booking
     @flight = @booking.flight
     mail(to: @passenger.email, subject: "Booking confirmed for flight: #{@flight.flight_number}")
