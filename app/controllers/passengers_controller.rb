85084ab31917eaf886bc154203bc5d1e04df9e35
harmolipi <nikob381@gmail.com>
2021-10-12 10:23:28 -0400

    Realized not using passengers_controller methods

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,11 +1,2 @@
 class PassengersController < ApplicationController
-  def create
-    @passenger = Passenger.new(passenger_params)
-  end
-
-  private
-
-  def passenger_params
-    params.require(:passenger).permit(:name, :email)
-  end
 end
